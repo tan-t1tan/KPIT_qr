205049673d8fe62be23b76f2b39e3c46391535c5
mrixxf <mrixxf@gmail.com>
2022-09-18 17:50:35 +0300

    global changes

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from telebot import types
 bot = telebot.TeleBot(keys.BOT_TOKEN)
 db = db_connect.Database()
 
-start_markup =
+start_markup = types.ReplyKeyboardMarkup(row_width=1)
+btn1 = types.KeyboardButton('Поехали!')
+start_markup.add(btn1)
 
 
 @bot.message_handler(commands=['start'])
@@ -16,20 +18,28 @@ def on_start(message):
     user_id = message.from_user.id
     username = message.from_user.username
     if username is None: username = 'None'
+    if db.user_exists(user_id):
+        bot.send_message(user_id, db.get_stage_hint(user_id), reply_markup=types.ReplyKeyboardRemove(selective=False))
+    else:
+        bot.send_message(message.from_user.id, values.start_message, reply_markup=start_markup)
+        db.add_user(user_id, username)
 
-    if message.text == '/start': # Start message
 
-        if db.user_exists(user_id):
-            bot.send_message(user_id, db.get_stage_hint(user_id))
-        else:
-            bot.send_message(message.from_user.id, values.start_message)
-            db.add_user(user_id, username)
-    else: # Check flag
-        if not db.user_exists(user_id):
-            bot.send_message(user_id, values.wrong_way)
-        flag = message.text[7:]
+
+@bot.message_handler(regexp='Поехали!')
+def first_hint(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, db.get_stage_hint(user_id), reply_markup=types.ReplyKeyboardRemove(selective=False))
+
+
+@bot.message_handler()
+def first_hint(message):
+    user_id = message.from_user.id
+    if not db.user_exists(user_id):
+        bot.send_message(user_id, values.incorrect_start)
+    else:
+        flag = message.text
         result = db.next_stage(user_id, flag)
-        print(result)
         if result == 0:
             bot.send_message(user_id, values.wrong_flag)
         if result == 1:
