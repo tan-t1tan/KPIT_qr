eb667f4d0725c5b0b9f4493728cc7e1756e1b9b0
mrixxf <mrixxf@gmail.com>
2022-09-18 15:20:48 +0300

    update

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ db = db_connect.Database()
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def on_start(message):
     user_id = message.from_user.id
-    # username = message.from_user.
-    if db.user_exists(user_id):
-        bot.send_message(user_id, 'Hi!')
-    else:
-        # db.add_user(user_id)
-        bot.send_message(message.from_user.id, values.start_message)
+    username = message.from_user.username
+
+    if message.text == '/start': # Start message
+        if username is None: username = 'None'
+
+        if db.user_exists(user_id):
+            bot.send_message(user_id, 'Hi!')
+        else:
+            bot.send_message(message.from_user.id, values.start_message)
+            db.add_user(user_id, username)
+    else: # Check flag
+        flag = message.text[6:]
+        result = db.next_stage(user_id, flag)
+
+
 
 
 if __name__ == '__main__':
